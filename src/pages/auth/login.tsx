73691c0ace3ab2986dcd806a0784975966ab6905
Hamdani <hamdanilombok@gmail.com>
2024-02-03 10:45:31 +0800

    update login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,17 @@
 import { LoginForm } from "@/features/auth";
 import { AuthLayout } from "@/layouts";
 import { NextPageWithLayout } from "@/types";
+import Head from "next/head";
 
 const Login: NextPageWithLayout = () => {
-  return <LoginForm />;
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <LoginForm />
+    </>
+  );
 };
 
 Login.getLayout = (page) => <AuthLayout>{page}</AuthLayout>;
